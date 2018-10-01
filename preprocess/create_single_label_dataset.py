aa06f82d5e77ede46af4b5d7d38551aaeec7c011
Suthee Chaidaroon <unsuthee@gmail.com>
2018-10-01 13:20:17 -0700

    add a script to create a single-labeled dataset

---
+++ preprocess/create_single_label_dataset.py
@@ -0,0 +1,107 @@
+import os
+import numpy as np
+import pandas as pd
+import pickle
+from tqdm import tqdm
+import argparse
+from sklearn.utils import shuffle
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+#from nltk.stem import PorterStemmer
+
+##################################################################################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dataset", help="Name of the dataset.")
+parser.add_argument('--remove_short_docs', dest='remove_short_docs', action='store_true', help='Remove any document that has a length less than 5 words.')
+parser.add_argument('--remove_long_docs', dest='remove_long_docs', action='store_true', help='Remove any document that has a length more than 500 words.')
+parser.set_defaults(remove_short_docs=True)
+parser.set_defaults(remove_long_docs=True)
+
+args = parser.parse_args()
+    
+if not args.dataset:
+    parser.error("Need to provide the dataset.")
+
+##################################################################################################
+remove_short_document = args.remove_short_docs
+remove_long_document = args.remove_long_docs
+
+if args.dataset == 'ng20':
+    train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes')) 
+    test = fetch_20newsgroups(subset='test', remove=('headers', 'footers', 'quotes'))
+    train_docs = train.data
+    train_tags = train.target
+    test_docs = test.data
+    test_tags = test.target
+    
+##################################################################################################
+
+count_vect = CountVectorizer(stop_words='english', max_features=10000, max_df=0.8, min_df=3)
+train_tf = count_vect.fit_transform(train_docs)
+test_tf = count_vect.transform(test_docs)
+
+def create_dataframe(doc_tf, doc_targets):
+    docs = []
+    for i, bow in enumerate(doc_tf):
+        d = {'doc_id': i, 'bow': bow, 'label': doc_targets[i]}
+        docs.append(d)
+    df = pd.DataFrame.from_dict(docs)
+    df.set_index('doc_id', inplace=True)
+    return df
+
+train_df = create_dataframe(train_tf, train_tags)
+test_df = create_dataframe(test_tf, test_tags)
+
+def get_doc_length(doc_bow):
+    return doc_bow.sum()
+
+# def get_num_word(doc_bow):
+#     return doc_bow.nonzero()[1].shape[0]
+
+# remove an empty document
+train_df = train_df[train_df.bow.apply(get_doc_length) > 0]
+test_df = test_df[test_df.bow.apply(get_doc_length) > 0]
+
+print('num train: {} num test: {}'.format(len(train_df), len(test_df)))
+
+if remove_short_document:
+    print('remove any short document that has less than 5 words.')
+    train_df = train_df[train_df.bow.apply(get_doc_length) > 5]
+    test_df = test_df[test_df.bow.apply(get_doc_length) > 5]
+    print('num train: {} num test: {}'.format(len(train_df), len(test_df)))
+
+if remove_long_document:
+    print('remove any long document that has more than 500 words.')
+    train_df = train_df[train_df.bow.apply(get_doc_length) <= 500]
+    test_df = test_df[test_df.bow.apply(get_doc_length) <= 500]
+    print('num train: {} num test: {}'.format(len(train_df), len(test_df)))
+    
+##################################################################################################
+
+# split test and cv
+num_train = len(train_df)
+num_test = len(test_df) // 2
+num_cv = len(test_df) - num_test
+
+print('train: {} test: {} cv: {}'.format(num_train, num_test, num_cv))
+
+test_df = shuffle(test_df)
+cv_df = test_df.iloc[:num_cv]
+test_df = test_df.iloc[num_cv:]
+
+##################################################################################################
+
+# save the dataframes
+save_dir = '../dataset/{}'.format(args.dataset)
+print('save tf dataset to {} ...'.format(save_dir))
+
+train_df.to_pickle(os.path.join(save_dir, 'train.tf.df.pkl'))
+test_df.to_pickle(os.path.join(save_dir, 'test.tf.df.pkl'))
+cv_df.to_pickle(os.path.join(save_dir, 'cv.tf.df.pkl'))
+
+# save vocab
+with open('../dataset/ng20/vocab.pkl', 'wb') as handle:
+    pickle.dump(count_vect.vocabulary_, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+print('Done.')
